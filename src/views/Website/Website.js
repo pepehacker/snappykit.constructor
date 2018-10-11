fa4954fa372f258a59de3b5b8c7134905ef51410
Ivan Vyatkin <vanya095@gmail.com>
2018-10-11 13:45:36 +0300

    Hotfix: delete console statement.

--- src/views/Website/Website.js
+++ src/views/Website/Website.js
@@ -11,27 +11,23 @@ import Editor from 'views/Editor';
 
 import styles from './Website.scss';
 
-const Website = ({ match, ...props }) => {
-  console.log(match);
-  console.log(url(match.url, '/editor'));
-  return (
-    <div className={styles.Root}>
-      <div className={styles.Menu}>
-        <Menu />
+const Website = ({ match }) => (
+  <div className={styles.Root}>
+    <div className={styles.Menu}>
+      <Menu />
 
-        <div className={styles.Copyright}>
-          © 2018 Snappykit. All rights reserved
-        </div>
-      </div>
-
-      <div className={styles.Container}>
-        <Switch>
-          <Route path={url(match.url, '/editor')} component={Editor} />
-        </Switch>
+      <div className={styles.Copyright}>
+        © 2018 Snappykit. All rights reserved
       </div>
     </div>
-  );
-};
+
+    <div className={styles.Container}>
+      <Switch>
+        <Route path={url(match.url, '/editor')} component={Editor} />
+      </Switch>
+    </div>
+  </div>
+);
 
 Website.propTypes = {
   match: PropTypes.shape({
